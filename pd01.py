3f16edacb5bb4af06188482a556eb1c06b66a198
ievaekarklina <ievakarklina746@gmail.com>
2024-11-08 11:44:45 +0200

    Add files via upload

---
+++ pd01.py
@@ -0,0 +1,17 @@
+x=int(input("x:"))
+y=int(input("y:"))
+results (2y**x(-144x+2))/(x+y)**2
+print (result1)
+
+x=int(input("x:"))
+y=int(input("y:"))
+results 4x**(y+3)+15y
+print (result2)
+
+x=int(input("x:"))
+y=int(input("y:"))
+result 2+x-2xy/y/x+y
+print (result3)
+
+
+float(input("x"))
